f5b607ff7698669007c1dfde10b13528b39e3a0a
n10710841 <sumavpe@gmail.com>
2022-10-05 22:26:43 +1000

    Set up the Flask basic

---
+++ event/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'junk'
+
+    from . import views
+    app.register_blueprint(views.mainbp)
+    return app
+
---
+++ event/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, session, request, redirect, url_for
+
+mainbp = Blueprint("main",__name__)
+
+@mainbp.route("/")
+def index():
+    return render_template('index.html')
+
+@mainbp.route("/login")
+def login():
+    return render_template('login.html')
+
+@mainbp.route("/handelLoginForm", methods=["POST"])
+def handelLoginForm():
+    session["email"] = request.values.get("email")
+    return redirect(url_for("main.index"))
+
+@mainbp.route("/logout")
+def logout():
+    session.pop("email")
+    return "User logged out"

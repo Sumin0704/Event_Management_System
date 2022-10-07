536aca5b062bb2b7f3277af93db172dca3795f91
n10710841 <sumavpe@gmail.com>
2022-10-07 17:22:27 +1000

    Build the create form page

--- event/forms.py
+++ event/forms.py
@@ -1,8 +1,17 @@
-#User comment
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField, FileRequired
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
+class SportForm(FlaskForm):
+  name = StringField("Country", validators=[InputRequired()])
+  # adding two validators, one to ensure input is entered and other to check if the
+  # description meets the length requirements
+  description = TextAreaField("Description", validators=[InputRequired()])
+  image = TextAreaField("Image", validators=[InputRequired()])
+  rating = StringField("Rating", validators=[InputRequired()])
+  submit = SubmitField("Create")
+
 class CommentForm(FlaskForm):
   text = TextAreaField('Comment', [InputRequired()])
-  submit = SubmitField('Create')
+  submit = SubmitField('Post')
--- event/sports.py
+++ event/sports.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, request, redirect, url_for
 from .models import Sport, Comment
-from .forms import CommentForm
+from .forms import SportForm, CommentForm
+from . import db
 
 mainbp = Blueprint("sport", __name__, url_prefix="/sports")
 
@@ -21,6 +22,21 @@ def get_sport():
     tennis.add_comment(comment1)
     tennis.add_comment(comment2)
     return tennis
+
+# Create the Sport event route
+@mainbp.route("/create", methods=["GET"])
+def create():
+    print("Method type: ", request.method)
+    form = SportForm()
+    return render_template("create_form.html", form=form)
+
+@mainbp.route("/create", methods=["POST"])
+def createFormSubmission():
+    form = SportForm()
+    if form.validate_on_submit():
+        print("Success to create")
+        # Always end with redirect when form is valid
+        return redirect(url_for("sport.create"))
 
 @mainbp.route("/<id>/comment", methods=["POST"])
 def comment(id):

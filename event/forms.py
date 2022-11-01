9a59f022c24c7ae027e1ea55c5a18ef45a6ed53c
Sumin Kim <n10701841@qut.edu.au>
2022-11-01 17:07:53 +1000

    Code review & merge it from edit branch

--- event/forms.py
+++ event/forms.py
@@ -1,9 +1,7 @@
-from email.policy import default
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField, FileRequired
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField,DateTimeLocalField, IntegerField, DecimalField, SelectField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, NumberRange
-
 
 ALLOWED_FILE = {"PNG", "JPG", "JPEG", "png", "jpg", "jpeg"}
 STATUS_CHOICES = [("Open", "Open"), ("Unpublished", "Unpublished"), ("Sold Out", "Sold Out"), ("Cancelled", "Cancelled")]
@@ -54,14 +52,35 @@ class EventForm(FlaskForm):
   startDateTime = DateTimeLocalField("Start Date and Time", format="%Y-%m-%dT%H:%M", validators=[InputRequired()])
   endDateTime = DateTimeLocalField("End Date and Time", format="%Y-%m-%dT%H:%M",validators=[InputRequired()])
   ticketsAvailable = IntegerField("Total Available Tickets",validators=[InputRequired(), NumberRange(min=0)])
-  price = StringField("Individual Ticket Price", validators=[InputRequired()])
+  price = DecimalField("Individual Ticket Price", validators=[InputRequired()])
   status = SelectField("Event Status", choices=STATUS_CHOICES, validators=[InputRequired()])
   submit = SubmitField("Create")
 
+class EditEventForm(FlaskForm):
+  name = StringField("Event Name", validators=[InputRequired()])
+  # adding two validators, one to ensure input is entered and other to check if the
+  # description meets the length requirements
+  type = SelectField("Event Type", choices=TYPE_CHOICES, validators=[InputRequired()])
+  location = StringField("Location", validators=[InputRequired()])
+  rating = StringField("Rating", validators=[InputRequired()])
+  description = TextAreaField("Description", validators=[InputRequired()])
+  image = FileField(
+        "Destination Image",
+        validators=[
+            FileAllowed(ALLOWED_FILE, message="Only supports png,jpg,JPG,PNG"),
+        ],
+    )
+  startDateTime = DateTimeLocalField("Start Date and Time", format="%Y-%m-%dT%H:%M", validators=[InputRequired()])
+  endDateTime = DateTimeLocalField("End Date and Time", format="%Y-%m-%dT%H:%M",validators=[InputRequired()])
+  ticketsAvailable = IntegerField("Total Available Tickets",validators=[InputRequired(), NumberRange(min=0)])
+  price = DecimalField("Individual Ticket Price", validators=[InputRequired()])
+  status = SelectField("Event Status", choices=STATUS_CHOICES, validators=[InputRequired()])
+  submit = SubmitField("Update Event")
+
 class CommentForm(FlaskForm):
-  text = TextAreaField('Comment', [InputRequired()], id="comment_input")
-  submit = SubmitField('Post', id="comment_submit")
+  text = TextAreaField('Comment', [InputRequired()])
+  submit = SubmitField('Post')
 
 class OrderForm(FlaskForm):
-    order_num_tickets = IntegerField("Tickets",validators=[InputRequired()], default=1)
-    submit = SubmitField('Order', id="order_submit")
+    order_num_tickets = IntegerField("Ticket Quantity",validators=[InputRequired()])
+    submit = SubmitField('Order')
